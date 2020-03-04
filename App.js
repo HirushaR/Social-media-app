51756f488e69cfc237ab34516d4a8910ea5753a9
HirushaR <hirusharandunu11@gmail.com>
2020-03-04 22:01:56 +0530

    add firebase config

--- App.js
+++ App.js
@@ -1,40 +1,20 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import {createStackNavigator} from 'react-native-stack'
+import loadingScreen from './screens/loadingscreen'
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
-class Blink extends Component {
+import * as firebase from 'firebase'
 
-  componentDidMount(){
-    // Toggle the state every second
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 1000);
-  }
-
-  //state object
-  state = { isShowingText: true };
-
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
-  render() {
-    return (
-      <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
-      </View>
-    );
-  }
-}
+var firebaseConfig = {
+  apiKey: "AIzaSyDJhxA55ogWh-MTOZ2ZsoOFdlASntQ9cr8",
+  authDomain: "socialapp-58122.firebaseapp.com",
+  databaseURL: "https://socialapp-58122.firebaseio.com",
+  projectId: "socialapp-58122",
+  storageBucket: "socialapp-58122.appspot.com",
+  messagingSenderId: "18538358585",
+  appId: "1:18538358585:web:910d3c53097ff557b5eae8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
